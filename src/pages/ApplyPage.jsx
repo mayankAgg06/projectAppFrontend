3b69cf21f06d8020edf967c06e0581263ac62068
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2024-06-17 02:28:59 +0530

    added redirections

--- src/pages/ApplyPage.jsx
+++ src/pages/ApplyPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect} from 'react';
 import { AuthContext } from '../context/AuthContext';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
