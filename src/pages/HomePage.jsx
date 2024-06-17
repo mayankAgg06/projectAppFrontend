e1c6aeb12aa15484dff95bee77d0ce322a7fd9cc
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2024-06-17 21:59:02 +0530

    Added Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/navbar"
+
 export default function HomePage(){
     return (
         <>
+        <Navbar />
         <h1>Internshala Internship portal</h1>
         <p>Apply to Exciting Opportunities, Go to Opportunities Page</p>
         </>
